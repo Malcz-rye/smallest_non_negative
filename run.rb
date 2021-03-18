7c934b0a12e7315dab42733dfadfb5f034ef1b57
realtradam <mr.malczewski@gmail.com>
2021-03-18 01:16:21 -0400

    .

--- run.rb
+++ run.rb
@@ -3,19 +3,47 @@
 # ruby run.rb 1 2 3 4
 
 # var setup
-sorted = ARGV.map(&:to_i)
+@sorted = ARGV.map(&:to_i)
 brute_answer = nil
 brute_time = 0
+@divide_and_conquer_answer = nil
+@divide_and_conquer_time = 0
 
 # brute force
-sorted.each_with_index do |item, index|
+@sorted.each_with_index do |item, index|
   brute_time += 1
   unless item == index
     brute_answer = index
     break
   end
 end
-brute_answer ||= sorted.size
+brute_answer ||= @sorted.size
+
+# divide and conquer
+def recurse(min, max)
+  @divide_and_conquer_time += 1
+  if min == max
+    if @sorted[min] == min
+      @divide_and_conquer_answer = min + 1
+    else
+      @divide_and_conquer_answer = min
+    end
+    return
+  end
+  middle = (((max - min) / 2) + min).to_i
+  if @sorted[middle] == middle
+    # Do right half
+    recurse(middle + 1, max)
+  else
+    # Do left half
+    recurse(min, middle)
+  end
+end
+recurse(0, @sorted.size - 1)
+
 
 # showing results
-puts "The brute forced answer is #{brute_answer} and took #{brute_time} amount of time"
+puts "The brute forced answer is #{brute_answer}" \
+     " and took #{brute_time} amount of time"
+puts "The divide and conquer answer is #{@divide_and_conquer_answer}" \
+     " and took #{@divide_and_conquer_time} amount of time"
